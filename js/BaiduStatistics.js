a34df7de555ad5f5bf31c53a9245faf35f0bd852
wencwcoder <wencwcoder@users.noreply.github.com>
2023-04-24 03:32:20 +0000

    deploy: 2595cf53d38b6f1fa0fd617ec6ec00afd7426c29

---
+++ js/BaiduStatistics.js
@@ -0,0 +1,10 @@
+// 百度统计代码
+// 其中 id 可以换成自己的百度统计 id，登录百度统计查看网站浏览记录
+// https://tongji.baidu.com/web/homepage/index
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e6e1107da2b963e38f6e35e369ceb7a6";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
